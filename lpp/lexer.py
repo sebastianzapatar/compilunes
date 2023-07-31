e7f9d50e53be98d8c9b5346e9b1da4faffc48af4
Sebastián Zapata <63472538+sebastianzapatar@users.noreply.github.com>
2023-07-31 09:15:16 -0500

    Se organiza la lectura

--- lpp/lexer.py
+++ lpp/lexer.py
@@ -8,24 +8,26 @@ class Lexer:
     def __init__(self,source:str) -> None:
         self._source:str=source
         self._character:str=''
-        self._read_positon:int=0
         self._position:int = 0
+        self._read_position:int=0
+        self._read_character()
         
 
     def next_token(self)->Token:
         if match(r'^=$',self._character):
             token=Token(TokenType.ASSING,self._character)
         elif match(r'^\+$',self._character):
-            token=Token (TokenType.PLUS,self._character)
-        elif match(r'^;$',self._character):
-            token=Token(TokenType.SEMICOLON,self._character)
-        else:
+            token=Token(TokenType.PLUS,self._character)
+        elif match(r'^$',self._character):
+            token=Token(TokenType.EOF,self._character)
+        else: 
             token=Token(TokenType.ILLEGAL,self._character)
+        self._read_character()
         return token
     def _read_character(self)->None:
-        if self._read_positon>=len(self._source):
+        if self._read_position>=len(self._source):
             self._character=''
         else:
-            self._character=self._source[self._read_positon]
-        self._position=self._read_positon
-        self._read_positon+=1
+            self._character=self._source[self._read_position]
+        self._position=self._read_position
+        self._read_position+=1
