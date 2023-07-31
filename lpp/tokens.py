b8cd4a3f7bf8e182ac6033b69abb81acfdb96161
Sebastián Zapata <63472538+sebastianzapatar@users.noreply.github.com>
2023-07-31 08:29:00 -0500

    Se crea el lexer

---
+++ lpp/tokens.py
@@ -0,0 +1,42 @@
+from enum import (
+    auto,
+    Enum,
+    unique,
+    Dict
+)
+from typing import NamedTuple
+
+@unique
+class TokenType(Enum):
+    ASSING=auto()
+    COMMA=auto()
+    EOF=auto()
+    FUNCTION=auto()
+    IDENT=auto()
+    ILLEGAL=auto()
+    INT=auto()
+    LBRACE=auto()
+    LET=auto()
+    LPAREN=auto()
+    MINUS=auto()
+    PLUS=auto()
+    RBRACE=auto()
+    RPAREN=auto()
+    SEMICOLON=auto()
+
+
+
+class Token(NamedTuple):
+    token_type:TokenType
+    literal:str
+    def __str__(self) -> str:
+        return f'Type {self.token_type}, Literal {self.literal}'
+    
+
+
+def lookup_token_type(literal:str)->TokenType:
+    keywords:Dict[str,TokenType]={
+        'variable':TokenType.LET,
+        'funcion':TokenType.FUNCTION
+    }
+    return keywords.get(literal,TokenType.IDENT)
