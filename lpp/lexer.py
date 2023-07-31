922cba909dff9e0bb17c993f881c182e3be879c8
Sebastián Zapata <63472538+sebastianzapatar@users.noreply.github.com>
2023-07-31 09:52:45 -0500

    Se crea para leer variable e identificadores

--- lpp/lexer.py
+++ lpp/lexer.py
@@ -1,7 +1,8 @@
 from re import match
 from lpp.tokens import(
     Token,
-    TokenType
+    TokenType,
+    lookup_token_type
 )
 
 class Lexer:
@@ -14,16 +15,28 @@ class Lexer:
         
 
     def next_token(self)->Token:
+        self._skip_whitespace()
         if match(r'^=$',self._character):
             token=Token(TokenType.ASSING,self._character)
         elif match(r'^\+$',self._character):
             token=Token(TokenType.PLUS,self._character)
         elif match(r'^$',self._character):
             token=Token(TokenType.EOF,self._character)
+        elif self._is_letter(self._character):
+            literal=self._read_identifier()
+            token_type=lookup_token_type(literal)
+            return Token(token_type,literal)
+
         else: 
             token=Token(TokenType.ILLEGAL,self._character)
         self._read_character()
         return token
+
+    def _is_letter(self,character:str)->bool:
+        return bool(match(r'^[a-z-A-Z]',character))
+    def _is_number(self,character:str)->bool:
+        return bool(match('^\d$', character ))
+        #return bool(match('^[0-9]$', character ))
     def _read_character(self)->None:
         if self._read_position>=len(self._source):
             self._character=''
@@ -31,3 +44,13 @@ class Lexer:
             self._character=self._source[self._read_position]
         self._position=self._read_position
         self._read_position+=1
+    def _read_identifier(self)->str:
+        initial_position=self._position
+        while self._is_letter(self._character):
+            self._read_character()
+        return self._source[initial_position:self._position]
+
+    def _skip_whitespace(self)->None:
+        while match(r'^\s$',self._character):
+            self._read_character()
+
