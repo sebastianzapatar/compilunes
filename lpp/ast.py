e2b7d4b62e838822c6f7c66c0c178eeea271b67c
Sebastián Zapata <63472538+sebastianzapatar@users.noreply.github.com>
2023-08-14 11:15:11 -0500

    Se crea el programa y los nodos del arbol

--- lpp/ast.py
+++ lpp/ast.py
@@ -3,3 +3,42 @@ from abc import(
     abstractmethod
 )
 from typing import List
+from lpp.tokens import Token
+
+class ASTNode(ABC):
+    
+    @abstractmethod
+    def token_literal(self)->str:
+        pass
+    @abstractmethod
+    def __str__(self) -> str:
+        pass
+
+class Statement(ASTNode):
+    def __init__(self,token:Token) -> None:
+        self.token=token
+
+    def token_literal(self) -> str:
+        return self.token.literal
+    
+class Expression(ASTNode):
+    def __init__(self,token:Token) -> None:
+        self.token=token
+
+    def token_literal(self) -> str:
+        return self.token.literal
+
+class Program(ASTNode):
+    """A program is a list of statements"""
+    def __init__(self,statements :List[Statement])->None:
+        self.statements=statements
+    def token_literal(self) -> str:
+        if len(self.statements)>0:
+            return self.statements[0].token_literal()
+        return ''
+    def __str__(self) -> str:
+        out:List[str]=[]
+        for statement in self.statements:
+            out.append(str(statement))
+        
+        return ''.join(out)
