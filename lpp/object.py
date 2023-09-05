ac7210110ffe29b68e1fd1ed605b9e781be9f8e7
Sebastián Zapata <63472538+sebastianzapatar@users.noreply.github.com>
2023-09-05 08:51:08 -0500

    Se crea la funcion para evaluar enteros

---
+++ lpp/object.py
@@ -0,0 +1,58 @@
+from abc import (
+    ABC,
+    abstractmethod,
+)
+from enum import (
+    auto,
+    Enum,
+)
+
+
+class ObjectType(Enum):
+    BOOLEAN = auto()
+    INTEGER = auto()
+    NULL = auto()
+
+
+class Object(ABC):
+
+    @abstractmethod
+    def type(self) -> ObjectType:
+        pass
+
+    @abstractmethod
+    def inspect(self) -> str:
+        pass
+
+
+class Integer(Object):
+
+    def __init__(self, value: int) -> None:
+        self.value = value
+
+    def type(self) -> ObjectType:
+        return ObjectType.INTEGER
+
+    def inspect(self) -> str:
+        return str(self.value)
+
+
+class Boolean(Object):
+
+    def __init__(self, value: bool) -> None:
+        self.value = value
+
+    def type(self) -> ObjectType:
+        return ObjectType.BOOLEAN
+
+    def inspect(self) -> str:
+        return 'verdadero' if self.value else 'falso'
+
+
+class Null(Object):
+
+    def type(self) -> ObjectType:
+        return ObjectType.NULL
+
+    def inspect(self) -> str:
+        return 'nulo'
